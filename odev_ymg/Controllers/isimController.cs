3158d9b4915b4be3f1b80d980f0e83f70a0d7d44
ilknurmrx <s.ilknur.ylmz@gmail.com>
2024-11-07 16:21:00 +0300

    Proje dosyası ekle.

---
+++ odev_ymg/Controllers/isimController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace odev_ymg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class isimController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public ActionResult<string> GetHelloWorld()
+        {
+            return "İlknur";
+        }
+    }
+}
