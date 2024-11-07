33854faf6f33e73354413c23db7abce9c751f140
ilknurmrx <s.ilknur.ylmz@gmail.com>
2024-11-07 21:42:00 +0300

    commit4

--- odev/commit.java
+++ odev/commit.java
@@ -2,28 +2,25 @@ import java.util.Scanner;
 
 public class commit {
     public static void main(String[] args) {
-        
+        //isim yazdirma
         System.out.println("İlknur"); 
-
-        
         Scanner scanner = new Scanner(System.in);
-
-      
+        
+        
+        //ortalama hesaplama
         System.out.print("Kaç sayının ortalamasını hesaplamak istiyorsunuz? ");
         int n = scanner.nextInt();
-
         double toplam = 0;
 
-        
         for (int i = 1; i <= n; i++) {
             System.out.print(i + ". sayıyı girin: ");
             double sayi = scanner.nextDouble();
             toplam += sayi;
         }
-
-       
+ 
         double ortalama = toplam / n;
         System.out.println("Girilen sayıların ortalaması: " + ortalama);
+
 
         //kullanıcının girdiği iki sayinin toplamı:
         System.out.print("birinci sayiyi giriniz: ");
@@ -44,7 +41,11 @@ public class commit {
         System.out.println("a*b= "+ carpim);
         scanner.close();
         
-        
+
+        //plaka kodunun degistirilmesi
+        System.out.print("burda plakayi 24 olarak degistiriyorum");
+        System.setProperty("server.port", "24");
+    
 
 
 
