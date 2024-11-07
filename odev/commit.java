cdba2b8b7f7ecebb918f34670d6893e4832b9b40
ilknurmrx <s.ilknur.ylmz@gmail.com>
2024-11-07 21:21:58 +0300

    commit3

--- odev/commit.java
+++ odev/commit.java
@@ -2,31 +2,54 @@ import java.util.Scanner;
 
 public class commit {
     public static void main(String[] args) {
-        // İsminizi yazdırma
-        System.out.println("İlknur"); // Kendi isminizi buraya yazın
+        
+        System.out.println("İlknur"); 
 
-        // Kullanıcıdan veri almak için Scanner nesnesi
+        
         Scanner scanner = new Scanner(System.in);
 
-        // Kullanıcıdan kaç sayının ortalamasını hesaplayacağını sor
+      
         System.out.print("Kaç sayının ortalamasını hesaplamak istiyorsunuz? ");
         int n = scanner.nextInt();
 
         double toplam = 0;
 
-        // Girilen sayıları toplama
+        
         for (int i = 1; i <= n; i++) {
             System.out.print(i + ". sayıyı girin: ");
             double sayi = scanner.nextDouble();
             toplam += sayi;
         }
 
-        // Ortalama hesaplama ve sonucu yazdırma
+       
         double ortalama = toplam / n;
         System.out.println("Girilen sayıların ortalaması: " + ortalama);
 
-        // Scanner nesnesini kapatma
+        //kullanıcının girdiği iki sayinin toplamı:
+        System.out.print("birinci sayiyi giriniz: ");
+        int sayi1 =scanner.nextInt();
+        System.out.print("ikinci sayiyi giriniz: ");
+        int sayi2 =scanner.nextInt();
+        int toplami = sayi1+sayi2;
+        System.out.println("sayi1+sayi2= "+ toplami);
+        
+
+
+        //kullanıcının girdiği iki sayinin carpimi:
+        System.out.print("birinci sayiyi giriniz: ");
+        int a =scanner.nextInt();
+        System.out.print("ikinci sayiyi giriniz: ");
+        int b =scanner.nextInt();
+        int carpim = a*b;
+        System.out.println("a*b= "+ carpim);
         scanner.close();
+        
+        
+
+
+
+
+
     }
 }
 
